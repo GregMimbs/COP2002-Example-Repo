51c6be8c6fb4ee1090cfdeb3871f8e58c283e09b
GregMimbs <144128356+GregMimbs@users.noreply.github.com>
2023-09-05 09:03:44 -0400

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,10 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Greg Mimbs")
+Greg Mimbs
+>>> print("COP2002-OM1")
+COP2002-OM1
+>>> print("Fall 2023")
+Fall 2023
+>>> print("This is an interactive session")
+This is an interactive session
